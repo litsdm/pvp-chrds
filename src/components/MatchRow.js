c9b08b3422a3183c6f05085774838b27dbe100f7
cdiezmoran <cdiezmoran@gmail.com>
2019-11-09 12:01:11 -0600

    Create match row component

---
+++ src/components/MatchRow.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import moment from 'moment';
+import { bool, string } from 'prop-types';
+import { momentObj } from 'react-moment-proptypes';
+
+const MatchRow = ({ username, expiryDate, score, clickable }) => {
+  const timeLeft = () => {
+    const now = moment();
+    const days = expiryDate.diff(now, 'days');
+    const hours = expiryDate.diff(now, 'hours');
+
+    if (days >= 1) return `${days}d`;
+    if (days < 1 && hours >= 1) return `${hours}h`;
+    return `${expiryDate.diff(now, 'minutes')}m`;
+  };
+
+  const children = (
+    <>
+      <View style={styles.leftSide}>
+        <Image
+          source={{ uri: 'https://thispersondoesnotexist.com/image' }}
+          style={styles.image}
+          resizeMode="cover"
+        />
+        <View style={styles.info}>
+          <Text style={styles.username}>{username}</Text>
+          <Text style={styles.date}>{timeLeft()} left to play</Text>
+        </View>
+      </View>
+      <View style={styles.rightSide}>
+        <View style={styles.category} />
+        <Text style={styles.score}>{score}</Text>
+      </View>
+    </>
+  );
+
+  return clickable ? (
+    <TouchableOpacity style={styles.container}>{children}</TouchableOpacity>
+  ) : (
+    <View style={styles.container}>{children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 12,
+    paddingBottom: 0
+  },
+  leftSide: {
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  rightSide: {
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  image: {
+    borderRadius: 60 / 2,
+    height: 60,
+    marginRight: 12,
+    width: 60
+  },
+  username: {
+    fontFamily: 'sf-medium',
+    fontSize: 16
+  },
+  date: {
+    fontFamily: 'sf-regular',
+    opacity: 0.4
+  },
+  category: {
+    backgroundColor: '#C4C4C4',
+    borderRadius: 6,
+    height: 36,
+    marginRight: 12,
+    width: 36
+  },
+  score: {
+    fontFamily: 'sf-regular',
+    fontSize: 18
+  }
+});
+
+MatchRow.propTypes = {
+  clickable: bool,
+  username: string.isRequired,
+  expiryDate: momentObj.isRequired,
+  score: string.isRequired
+};
+
+MatchRow.defaultProps = {
+  clickable: false
+};
+
+export default MatchRow;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,82 +1,98 @@
 import React from 'react';
-import { Image, StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View
+} from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 import { FontAwesome5 } from '@expo/vector-icons';
+// import moment from 'moment';
 
 import AnimatedCircle from '../components/AnimatedCircle';
 import ProgressBar from '../components/LevelProgressBar';
+// import MatchRow from '../components/MatchRow';
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.statusBar} />
-      <View style={styles.header}>
-        <AnimatedCircle
-          color="#7C4DFF"
-          size={152}
-          animationType="position-opacity"
-          endPosition={{ y: 152 - 152 / 4, x: -152 + 152 / 3 }}
-          circleStyle={{ right: -152, top: -152 }}
-        />
-        <AnimatedCircle
-          color="#FFC107"
-          size={115}
-          animationType="position-opacity"
-          delay={150}
-          endPosition={{ y: 115 - 115 / 3.5, x: 0 }}
-          circleStyle={{ top: -115, right: 42 }}
-        />
-        <AnimatedCircle
-          color="#FF5252"
-          size={90}
-          animationType="position-opacity"
-          delay={300}
-          endPosition={{ y: 90 - 90 / 1.8, x: 0 }}
-          circleStyle={{ top: -90, left: 42 }}
-          empty
-        />
-        <View style={styles.leftSide}>
-          <Text style={styles.greeting}>Hello Pam,</Text>
-          <Text style={styles.subtitle}>Ready to Play?</Text>
-        </View>
-        <View style={styles.rightSide}>
-          <TouchableOpacity style={styles.imgButton}>
-            <Image
-              resizeMode="cover"
-              source={{ uri: 'https://thispersondoesnotexist.com/image' }}
-              style={styles.profilePic}
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.info}>
-        <View style={styles.levelSection}>
-          <Text style={styles.lvlTxtWrapper}>
-            Lvl <Text style={styles.lvlTxt}>24</Text>
-          </Text>
-          <ProgressBar progress={50} />
-          <Text style={styles.progressTxt}>10 until next level</Text>
-        </View>
-        <View style={styles.verticalDivider} />
-        <View style={styles.moneySection}>
-          <View style={styles.coinWrapper}>
-            <FontAwesome5 name="coins" size={30} color="#FFC107" />
-            <Text style={styles.coins}>
-              10,000 <Text style={styles.coinWord}>coins</Text>
-            </Text>
+    <ScrollView>
+      <View style={styles.container}>
+        {Platform !== 'ios' ? <View style={styles.statusBar} /> : null}
+        <View style={styles.header}>
+          <AnimatedCircle
+            color="#7C4DFF"
+            size={152}
+            animationType="position-opacity"
+            endPosition={{ y: 152 - 152 / 4, x: -152 + 152 / 3 }}
+            circleStyle={{ right: -152, top: -152 }}
+          />
+          <AnimatedCircle
+            color="#FFC107"
+            size={115}
+            animationType="position-opacity"
+            delay={150}
+            endPosition={{ y: 115 - 115 / 3.5, x: 0 }}
+            circleStyle={{ top: -115, right: 42 }}
+          />
+          <AnimatedCircle
+            color="#FF5252"
+            size={90}
+            animationType="position-opacity"
+            delay={300}
+            endPosition={{ y: 90 - 90 / 1.8, x: 0 }}
+            circleStyle={{ top: -90, left: 42 }}
+            empty
+          />
+          <View style={styles.leftSide}>
+            <Text style={styles.greeting}>Hello Pam,</Text>
+            <Text style={styles.subtitle}>Ready to Play?</Text>
           </View>
-          <TouchableOpacity style={styles.getMore}>
-            <Text style={styles.getMoreText}>Get More</Text>
-          </TouchableOpacity>
+          <View style={styles.rightSide}>
+            <TouchableOpacity style={styles.imgButton}>
+              <Image
+                resizeMode="cover"
+                source={{ uri: 'https://thispersondoesnotexist.com/image' }}
+                style={styles.profilePic}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.info}>
+          <View style={styles.levelSection}>
+            <Text style={styles.lvlTxtWrapper}>
+              Lvl <Text style={styles.lvlTxt}>24</Text>
+            </Text>
+            <ProgressBar progress={90} />
+            <Text style={styles.progressTxt}>10 until next level</Text>
+          </View>
+          <View style={styles.verticalDivider} />
+          <View style={styles.moneySection}>
+            <View style={styles.coinWrapper}>
+              <FontAwesome5 name="coins" size={30} color="#FFC107" />
+              <Text style={styles.coins}>
+                10,000 <Text style={styles.coinWord}>coins</Text>
+              </Text>
+            </View>
+            <TouchableOpacity style={styles.getMore}>
+              <Text style={styles.getMoreText}>Get More</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.lists}>
+          {/* render lists when data is available */}
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: getStatusBarHeight()
+    paddingTop: getStatusBarHeight(),
+    paddingBottom: 24
   },
   statusBar: {
     backgroundColor: '#fff',
@@ -177,6 +193,21 @@ const styles = StyleSheet.create({
   getMoreText: {
     fontFamily: 'sf-medium',
     color: '#fff'
+  },
+  lists: {
+    paddingHorizontal: 24
+  },
+  title: {
+    fontFamily: 'sf-medium',
+    fontSize: 30,
+    marginTop: 24,
+    opacity: 0.6
+  },
+  divider: {
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    height: 1,
+    marginTop: 12,
+    width: '100%'
   }
 });
 
